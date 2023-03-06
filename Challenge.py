d11523c32a0c97a74397e789018c6ac99c94411d
Orbitanel <39680802+Orbitanel@users.noreply.github.com>
2023-03-06 18:32:14 -0400

    Add files via upload

---
+++ Challenge.py
@@ -0,0 +1,40 @@
+#*****TASK 1*****
+
+#Run this code.
+#It won't work.
+#Fix it by defining the variable myname and setting it equal to Jarvis
+
+myname = "Jarvis"
+
+print("Hello, my name is " + myname + ", what is your name?")
+
+
+
+#*****TASK 2******
+
+#The following variable is defined.
+
+#whatisyourname = "Ordnael"
+
+#Have Jarvis (the computer) ask you what your name is and set it equal to the variable above.
+
+#put your code below here ↓↓↓↓
+
+#uncomment the next line of code and click Run. Did it work?
+
+#print("Hello " + whatisyourname + ", it is so nice to meet you.")
+
+#*****TASK 3*****
+
+#Uncomment the following lines of code. Click Run.
+#Now fix the code.
+
+wizard = "Harry"
+
+print("You're a wizard " + wizard + ".")
+
+yourname = input("What is your name? \n")
+
+yourhouse = input("Hello " + yourname + ", what house are you in?\n \n" + "Gryffindor, HufflePuff, Ravenclaw, Slytherin? ")
+
+print("Welcome to the Leaky Cauldron, " + yourname + " of " + yourhouse + ".")
---
+++ Netbarista.py
@@ -0,0 +1,11 @@
+#Let's start a coffee shop together!! We're going to build a coffee shop using some new Python programming concepts!!
+
+#Let's build robot Barista
+
+print("Hello, welcome to NetworkChuck Coffee !!!!!!!!!!")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + ", thank you so much for coming in today")
+
+
